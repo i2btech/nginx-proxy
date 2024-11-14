e3cb5039f29887f046fb8bd3e4aa4f1a06dffc9c
alchandia <alchandia@gmail.com>
2024-11-14 10:51:07 -0300

    refactor: get domain(s) dynamically

--- certbot.sh
+++ certbot.sh
@@ -2,8 +2,19 @@
 set -e
 
 if [ -f /devops/nginx-final.conf ] ; then
-    SSL_KEY=$(cat /devops/nginx-final.conf | grep ssl_certificate_key | tr -s " " | cut -d " " -f 3 | cut -d ";" -f 1)
+
+    cat /devops/nginx-final.conf | grep ssl_certificate_key | tr -s " " > certbot_domains.txt
+    while read line ; do
+        if [ -z $CERTBOT_DOMAINS ] ; then
+            CERTBOT_DOMAINS=$(echo -e "$line" | tr -s " " | cut -d " " -f 2 | cut -d ";" -f 1 | cut -d "/" -f 5)
+        else
+            CERTBOT_DOMAINS="$CERTBOT_DOMAINS,$(echo -e "$line" | tr -s " " | cut -d " " -f 2 | cut -d ";" -f 1 | cut -d "/" -f 5)"
+        fi
+    done < certbot_domains.txt
+
+    SSL_KEY=$(cat /devops/nginx-final.conf | grep ssl_certificate_key | tr -s " " | cut -d " " -f 3 | cut -d ";" -f 1 | tail -n 1)
     if [ -f $SSL_KEY ] ; then
+        # if there is multiple vhost and one certificate exist, we assume all of them exist
         echo "key found" > /devops/certbot.log
         exit 1
     else
@@ -14,7 +25,7 @@ if [ -f /devops/nginx-final.conf ] ; then
             --agree-tos \
             --no-eff-email \
             --no-redirect \
-            --email "${CERTBOT_EMAIL}" \
+            --register-unsafely-without-email \ 
             --domain "${CERTBOT_DOMAINS}"
         sleep 3
         rm -rf /etc/nginx/sites-enabled/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,9 @@ mkdir /mnt/data/letsencrypt
 ln -s /mnt/data/letsencrypt /etc/letsencrypt
 
 if [ -f /devops/nginx-final.conf ] ; then
-    SSL_KEY=$(cat /devops/nginx-final.conf | grep ssl_certificate_key | tr -s " " | cut -d " " -f 3 | cut -d ";" -f 1)
+    SSL_KEY=$(cat /devops/nginx-final.conf | grep ssl_certificate_key | tr -s " " | cut -d " " -f 3 | cut -d ";" -f 1 | tail -n 1)
     if [ -f $SSL_KEY ] ; then
+        # if there is multiple vhost and one certificate exist, we assume all of them exist
         cp /devops/nginx-final.conf /etc/nginx/sites-enabled/
     else
         cp /devops/nginx-initial.conf /etc/nginx/sites-enabled/
